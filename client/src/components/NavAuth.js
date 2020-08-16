45356ad93c56957566fffac654c4cd3657129bb5
azar103 <azzarroukanis@gmail.com>
2020-08-16 23:40:31 +0100

    commit

--- client/src/components/NavAuth.js
+++ client/src/components/NavAuth.js
@@ -1,74 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { getLengthOfCart } from '../Helpers/functions'
-import Logout from './Logout'
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { getLengthOfCart } from "../Helpers/functions";
+import Logout from "./Logout";
 
+function NavAuth(props) {
+  const getLength = () => {
+    if (props.user === null) {
+      return 0;
+    } else {
+      return getLengthOfCart(props.items, props.user);
+    }
+  };
 
+  return (
+    <>
+      <li id="left">
+        <Link to="/">FoodDelivery</Link>
+      </li>
+      <div id="right">
+        {props.auth && props.user && props.profile !== null && (
+          <li>{`Welcome, ${props.profile[0].firstName}`}</li>
+        )}
 
+        <li>
+          <a href="#footer">Contact</a>
+        </li>
 
-
- function NavAuth(props) {
-    const getLength = () => {
-        if(props.user === null) {
-            return 0
-        }else {
-            return getLengthOfCart(props.items, props.user)
-        }
-    }    
-
-    return (
-        <>
-             <li id="left">
-                  <Link to="/">FoodDelivery</Link>
-              </li>    
-              <div id="right"> 
-                {
-                    props.auth && props.user&& props.profile!==null&&
-                    <li>{`Welcome, ${props.profile[0].firstName}`}</li> 
-                } 
-        
-                 <li>
-                    <a href="#footer" >Contact</a>
-                 </li>   
-     
-
-                 {
-                    getLength() > 0
-                    ?
-                   props.auth &&
-                    <li>
-                
-                    <div className="notification">{getLength()}</div>                 
-                    <Link to="/orders"><i className="fa fa-shopping-cart"></i></Link>
-                   </li>  
-                  :
-                  props.auth &&
-                  <li>                  
-                  <Link to="/orders"><i className="fa fa-shopping-cart"></i></Link>
-                 </li>  
-                 } 
-                {
-               props.auth === true ?
-                 <Logout /> 
-                 :
-                 <li>
-                    <Link to="/login">Login</Link>
-                 </li> 
-                } 
-              </div>  
-        </>
-    )
+        {getLength() > 0
+          ? props.auth && (
+              <li>
+                <div className="notification">{getLength()}</div>
+                <Link to="/orders">
+                  <i className="fa fa-shopping-cart"></i>
+                </Link>
+              </li>
+            )
+          : props.auth && (
+              <li>
+                <Link to="/orders">
+                  <i className="fa fa-shopping-cart"></i>
+                </Link>
+              </li>
+            )}
+        {props.auth === true ? (
+          <Logout />
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+      </div>
+    </>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        items: state.cartReducer.items,
-        auth: state.authReducer.isAuthenticated,
-        profile: state.userReducer.user,
-        user: state.authReducer.user,
-    
-    }
-}
+  return {
+    items: state.cartReducer.items,
+    auth: state.authReducer.isAuthenticated,
+    profile: state.userReducer.user,
+    user: state.authReducer.user,
+  };
+};
 
-export default connect(mapStateToProps)(NavAuth)
+export default connect(mapStateToProps)(NavAuth);
