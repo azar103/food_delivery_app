e925cce317edfb90b6b0f50bde32637e39ee391e
azar103 <azzarroukanis@gmail.com>
2020-08-16 16:10:19 +0100

    deleter clear all button

--- client/src/components/Orders.js
+++ client/src/components/Orders.js
@@ -1,86 +1,69 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import './Orders.scss'
-import Order from './Order'
-import swal from 'sweetalert'
-import Nav from './Nav'
-import { getLengthOfCart, cartFilteredById, totalPrice } from '../Helpers/functions'
-import NavAuth from './NavAuth'
-import { RemoveFromCart, clearCart } from '../reducers/cartActions'
-import Footer from './Footer'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import "./Orders.scss";
+import Order from "./Order";
+import swal from "sweetalert";
+import Nav from "./Nav";
+import {
+  getLengthOfCart,
+  cartFilteredById,
+  totalPrice,
+} from "../Helpers/functions";
+import NavAuth from "./NavAuth";
+import { RemoveFromCart, clearCart } from "../reducers/cartActions";
+import Footer from "./Footer";
 
-
-
- function Orders(props) {
-   
+function Orders(props) {
   const getLength = () => {
-    if(props.user === null) {
-        return 0
-    }else {
-        return getLengthOfCart(props.items, props.user)
+    if (props.user === null) {
+      return 0;
+    } else {
+      return getLengthOfCart(props.items, props.user);
     }
-}    
+  };
 
+  const cartFiltered = cartFilteredById(props.items, props.user.id);
+  const onDelete = (id) => {
+    props.dispatch(RemoveFromCart(id));
+  };
+  console.log(props.items);
+  return getLength() > 0 ? (
+    <>
+      <Nav>
+        <NavAuth />
+      </Nav>
+      <div className="orders_container">
+        <h2>{getLength()} command(s)</h2>
+        {cartFiltered.map((order, index) => (
+          <Order key={index} order={order} onDelete={onDelete} />
+        ))}
+      </div>
+      <Footer />
+    </>
+  ) : (
+    <>
+      <Nav>
+        <NavAuth />
+      </Nav>
+      <img src="../../no_commands.jpg" alt="no_image" className="img_order" />
 
-
-   const cartFiltered = cartFilteredById(props.items, props.user.id)
- const onDelete = id => {
-     props.dispatch(RemoveFromCart(id))
- }
-console.log(props.items);
-    return(
-        
-        getLength()>0
-        ?    
-        <>
-        <Nav >
-          <NavAuth />
-        </Nav> 
-        <div className="orders_container">
-             <h2>{getLength()} command(s)</h2>
-             {cartFiltered.map((order, index) => <Order 
-               key={index}
-                order={order}
-                onDelete = {onDelete}
-             />)        
-             }
-         {/**
-          * commit
-          */}
-            <p><b>Total Price:</b> {parseFloat(totalPrice(props.items.filter(item => item.userId === props.user.id))).toFixed(3)} DT</p>
-            <button
-              onClick={() => props.dispatch(clearCart())}
-              >CLEAR ALL</button>
-  
-        </div>
-        <Footer />  
-        </>
-        :
-        <>
-        <Nav >
-          <NavAuth />
-        </Nav>    
-        <img src="../../no_commands.jpg" alt="no_image"
-        className="img_order"
-        />
-        
-        {/**
+      {/**
          * <button
               onClick={() => props.dispatch(clearCart())}
               >CLEAR ALL</button>
          * 
-         */}  
-        <Footer />      
-        </>
-    )
+         */}
+      <Footer />
+    </>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        items: state.cartReducer.items,
-        auth: state.authReducer.isAuthenticated,
-        profile: state.userReducer.users,
-        user: state.authReducer.user
-    }
-}
-export default connect(mapStateToProps)(Orders)
+  return {
+    items: state.cartReducer.items,
+    auth: state.authReducer.isAuthenticated,
+    profile: state.userReducer.users,
+    user: state.authReducer.user,
+  };
+};
+export default connect(mapStateToProps)(Orders);
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userCtrl = require('../controllers/user');
-router.get('/', userCtrl.getUsers)
-router.post('/', userCtrl.createUser);
-router.post('/login', userCtrl.login);
-router.delete('/:id', userCtrl.deleteUser);
-router.get('/user/:id', userCtrl.getUser);
-
-
+const userCtrl = require("../controllers/user");
+router.get("/", userCtrl.getUsers);
+router.post("/", userCtrl.createUser);
+router.post("/login", userCtrl.login);
+router.delete("/:id", userCtrl.deleteUser);
+router.get("/user", userCtrl.getUser);
 
 module.exports = router;
